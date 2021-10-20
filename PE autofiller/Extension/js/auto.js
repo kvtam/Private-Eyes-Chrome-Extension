68aed412443df6cda006acb211b20ef0e91ef585
kvtam <kvtam@alaska.edu>
2021-10-20 15:44:47 +0900

    Regex edits

--- PE autofiller/Extension/js/auto.js
+++ PE autofiller/Extension/js/auto.js
@@ -1,4 +1,4 @@
-	var rt //Global String to store stuff to be translated
+
 	
 	function spaceToComma(string,target,replacement){ //Function to replace spaces in a string to commas
 		var i = 0, length = string.length;
@@ -23,6 +23,7 @@
 		document.getElementById('CatalogBoxDescription').value= fooDes;
 		document.getElementById('CatalogBoxEnDescription').value= fooDes;
 		var jptext= document.getElementById('CatalogBoxHtml1').value;
+		fieldsToCap();
 		caseEditor();
 		
 		var pageNum=window.location.href;
@@ -31,11 +32,25 @@
 		var num= /(?<=\/)\d+(?=\/)/.exec(pageNum);
 		//var num=pageNum.replace(/[\D]/g,'');
 			//alert(num);
-		alert(document.getElementById('CatalogBoxText1').value +","+num);
+		//alert(document.getElementById('CatalogBoxText1').value +","+num);
 		
 		
 	}
-	function yearFormat(str){
+	function fieldsToCap(){
+		const _ENFields=[
+		"CatalogBoxEnText8",
+		"CatalogBoxEnText9",
+		"CatalogBoxEnText11",
+		"CatalogBoxEnText12",
+		"CatalogBoxEnText13"
+		];
+		for(i=0;i<_ENFields.length;i++){
+			var temp=document.getElementById(_ENFields[i]).value;
+			temp=temp.replace(/^[a-z]/,match=>match.toUpperCase());
+			document.getElementById(_ENFields[i]).value=temp;
+		}
+	}
+	function yearFormat(str){ //Function to parse and format the year properly
 		//regexes
 		var year = /^(\d{2,4}['|’|`]s?)|(\d{2,4}(?=s))/i;
 		var multiYear=/^\d+-\d+['|’|`]?s?/i;
@@ -45,7 +60,7 @@
 		var yearCorrected;
 		if(!multiYear.test(str)){//If it's not a multi year do this
 			yearCorrected= year.exec(str);//format the year correctly
-			if(!(/19/.test(yearCorrected))){
+			if(!(/(19|18)/.test(yearCorrected))){
 				//if year doesn't start with 19 then add 19 to the front
 				yearCorrected = "19"+yearCorrected;
 				
@@ -79,26 +94,30 @@
 		return str.replace(multiYear,yearCorrected);
 		
 	}
-	function nameParser(){
+	function nameParser(){ //Function to parse and format the 'name' box on the Peyes webpage
 		//Regexes here
 		var steel = /(?<=\s)ss(?=\s|$)|S\.Steel/i;
 		var water = /(?<=\s)w\/p(?=\s)/i;
+		var WP= /Water\s?Proof/ig;
 		var black = /\s?BK/g;
 		var rose = /\s?RG/g;
-		var yellow= /\s?YG\s/g;
+		var yellow= /\s?YG(\s|$)/ig;
 		var yGF=/\s?YGF/ig;
 		var GF=/(?<!Y)GF/ig;
-		var pink=/\s?PG/g;
+		var pink=/\s?PG/ig;
 		var multi=/multi\s/ig;
 		var overSize=/over size/ig;
-		var toCap=/(?<=\s)[a-z](?=[a-z]{2})/g;
+		var toCap=/(?<=[\s])[a-z](?=[a-z]{2})/g;
 		var BE=/Bull's Eye/;
-		var toLow=/(?<=[A-Z])[A-Z]*(?=.)/g;
+		var toLow=/(?<=[A-Z])([A-Z])*(?=.)?/g;
 		var IWC=/IWC/ig;
 		var Ex=/\!/g;
 		var fullRotate=/Full Rotating/gi;
 		var screwBack=/Screw Back/gi;
 		var artDeco=/art deco/gi;
+		var PW =/P[,\.\/]?W/ig;
+		var WRB=/W,\s?Rosch Bern/ig;
+		
 	
 		
 		
@@ -106,8 +125,9 @@
 		//Array to store tokens
 		const rxTokens = [
 		[steel,"Stainless Steel"],
-		[water,"Waterproof"],
-		[black,"Black"],
+		[water,"Water-resistant"],
+		[WP,"Water-resistant"],
+		[black," Black"],
 		[rose," Rose Gold"],
 		[yellow," Yellow Gold "],
 		[yGF," Yellow Gold Filled"],
@@ -119,7 +139,16 @@
 		[screwBack,"Screw-back"],
 		[GF,"Gold Filled"],
 		[artDeco,"Art-deco"],
-		[IWC,"IWC"]
+		[IWC,"IWC"],
+		[PW,"Pocket Watch"],
+		[WRB,"W. Rosch Bern"]
+		];
+		
+		const rxAN=[
+		/USN BUSHIPS/ig,
+		/USA/ig,
+		/IWC/ig,
+		/UFO/ig
 		];
 		
 		var name = document.getElementById('CatalogBoxName').value;//get the text from the namebox
@@ -131,19 +160,24 @@
 		for(i=0;i<rxTokens.length;i++){
 			name=name.replace(rxTokens[i][0],rxTokens[i][1]);
 		}
+		//Lowercase the letters not at the start of a word
 		name=name.replace(toLow,match => match.toLowerCase());
+		for(i=0;i<rxAN.length;i++){
+			name=name.replace(rxAN[i],match=>match.toUpperCase());
+		}
 		//At this point all of the tokens should have been replaced
 		document.getElementById('CatalogBoxName').value=name;
 	}
-	function tagFormat(str){
+	function tagFormat(str){ //Function to format the tags
 		//regexes
 		var tagSteel = /Stainless,Steel/;
 		var tagRose =/Rose,Gold/;
 		var tagYellow= /Yellow,Gold(?=!Filled)?/;
 		var tagPink=/Pink,Gold/;
+		var tagWhite=/White,Gold/;
 		var tagCaliber=/\d*mm,Caliber/i;
 		var tagGilt=/Gilt,dial/i;
-		var tagQuote=/"/g;
+		var tagQuote=/["”“]/g;
 		var tagSM300=/Seamaster,300/;
 		var tagCross=/Cross,Design/;
 		var tagAuto=/Automatic,Model/;
@@ -166,7 +200,43 @@
 		var tagAdmiral=/Admiralty,Model/i;
 		var tagMarine=/Marine,Standard/i;
 		var tagGF = /Gold,Filled/i;
+		var tagCoax=/Co-axial,Chronograph/i;
+		var tagWRB=/W.,Rosch,Bern/i;
+		var tagSC=/Super,Contiki/i;
+		var tagSB=/(Stepped,Bezel(?!,Chrono))|(Step,Bezel)/i;
+		var tagSBC=/Stepped,Bezel,Chrono/i;
+		var tagEP=/El,Primero/i;
+		var tagTLCF=/Technicum,La,Chaude,Fonds/i;
+		var tagWWC=/Wyler Watch,Co,/i;
+		var tagRD=/Roman,Dial/;
+		var tagMB=/Music,Box/;
+		var tagKMS=/Kronometer,Stockholm/;
+		var tagBD=/Bomb,Drop/;
+		var tagCAR=/Cronografo,A,Ritorno/;
+		var tagSplitSec=/Split,Second/;
+		var tagNiCr=/Nickel,Chrome/;
+		var tagPort=/Port,Royal/;
+		var tagBEye=/Big,Eye/;
+		var tagEBC=/Eberhard,&,Co/ig;
+		var tagEFort=/Extra,Fort/i;
+		var tagTudor=/Tudor,Oyster/;
+		var tagUSN=/USN,BUSHIPS/;
+		var tagMStar=/Marine,Star/;
+		var tagSGuide=/Sherpa,Guide/;
+		var tagWTime=/World,Time/;
+		var tagRGrasp=/Royal,Grasp/ig;
+		var tagEPark=/Excelsior,Park/;
+		var tagLReverso=/Lady,Reverso/;
+		var tagEGUB=/E,,Gubelin/;
+		var tagALung=/Aqua,Lung/;
+		var tagSeaSky=/Sea,Sky/;
+		var tagHMC=/H.,Moser,&,Cie/;
+		var tagEBorel=/Ernest,Borel/;
+		var tagGSport=/Geneva,Sport/i;
+		var tagFFAthoms=/Fifty,Fathoms/ig;
 		
+		
+		//Array to hold the tag tokens
 		const rxTokens_tags = [
 		[tagQuote,''],
 		[tagSteel,"Stainless Steel"],
@@ -192,10 +262,46 @@
 		[tagSandDial,"Sandwich Dial"],
 		[tagMarine,"Marine Standard"],
 		[tagGF, "Gold Filled"],
-		[tagYGF, "Yellow Gold Filled"]
+		[tagYGF, "Yellow Gold Filled"],
+		[tagCoax, "Co-axial Chronograph"], 
+		[tagWRB, "W. Rosch Bern"],
+		[tagSC,"Super Contiki"],
+		[tagSB,"Stepped Bezel"],
+		[tagSBC,"Stepped Bezel Chrono"],
+		[tagEP, "El Primero"],
+		[tagTLCF,"Technicum La Chaude Fonds"],
+		[tagWWC,"Wyler Watch Co.,"],
+		[tagRD,"Roman Dial"],
+		[tagMB,"Music Box"],
+		[tagBD,"Bomb Drop"],
+		[tagKMS,"Kronometer Stockholm"],
+		[tagCAR,"Cronografo A Ritorno"],
+		[tagSplitSec,"Split Second"],
+		[tagNiCr,"Nickel Chrome"],
+		[tagPort,"Port Royal"],
+		[tagBEye,"Big Eye"],
+		[tagEBC,"Eberhard & Co."],
+		[tagEFort,"Extra Fort"],
+		[tagTudor,"Tudor Oyster"],
+		[tagUSN,"USN BUSHIPS"],
+		[tagMStar,"Marine Star"],
+		[tagSGuide,"Sherpa Guide"],
+		[tagWTime, "World Time"],
+		[tagRGrasp,"Royal Grasp"],
+		[tagEPark,"Excelsior Park"],
+		[tagLReverso,"Lady Reverso"],
+		[tagEGUB,"E. Gubelin"],
+		[tagALung,"Aqua Lung"],
+		[tagSeaSky,"Sea Sky"],
+		[tagHMC, "H. Moser & Cie"],
+		[tagEBorel,"Ernest Borel"],
+		[tagGSport,"Geneva Sport"],
+		[tagFFAthoms,"Fifty Fathoms"],
+		[tagWhite,"White Gold"]
 		];
+		//Functions for commonly modified nouns
 		if(tagWatch.test(str)){
-			str=str.replace(/,Watch/," Watch");
+			str=str.replace(/,Watch/g," Watch");
 		}
 		if(tagLug.test(str)){
 			str=str.replace(/,Lug/," Lug");
@@ -206,16 +312,22 @@
 		if(tagIndex.test(str)){
 			str=str.replace(/,Index/," Index");
 		}
+		if(/(?<=,)Case/.test(str)){
+			str=str.replace(/,Case/," Case");
+		}
+		if(/(?<=,)Military/.test(str)){
+			str=str.replace(/,Military/," Military");
+		}
 		
 			
-		for(i=0;i<rxTokens_tags.length;i++){
+		for(i=0;i<rxTokens_tags.length;i++){//Replace tags here
 			str=str.replace(rxTokens_tags[i][0],rxTokens_tags[i][1]);
 		}
 		
 		return tagRemove(str);
 	}
-	function tagRemove(str){
-		
+	function tagRemove(str){ //Function to Remove bad tags
+		// Regexes
 		var rmIn = /(?<=,)in,/ig;
 		var rmWith= /(?<=,)with,/ig;
 		var rmDesign=/(?<=,)design,|,Design(?=$)/ig;
@@ -223,8 +335,10 @@
 		var rmComma=/(?<=,),*(?=$)?/g;
 		var rmDial=/(?<=,)Dial,|,Dial(?=$)/ig;
 		var rmModel=/(?<=,)Model,|,Model(?=$)/g;
+		var rmBy =/(?<=,)By,/ig;
+		var rmAmp=/(?<=,)&,/g;
 
-		
+		//Array of tokens to remove
 		const rmTokens=[
 		rmIn,
 		rmWith,
@@ -232,7 +346,9 @@
 		rmRare,
 		rmDial,
 		rmModel,
-		rmComma
+		rmComma,
+		rmBy,
+		rmAmp
 		]
 		for(i=0;i<rmTokens.length;i++){
 			str=str.replace(rmTokens[i],'');
@@ -241,6 +357,7 @@
 		return str;
 	}
 	//TODO: make this function work
+	/*
 	function translator(inputText){ //Function to translate text and put into string using the deepL api 
 		var url = "https://api-free.deepl.com/v2/translate";
 
@@ -259,9 +376,11 @@
 
 		xhr.send(data);
 	}
-	function caseEditor(){
+	*/
+	function caseEditor(){ //Function to edit the 'case fields in both English and Japanese'
 		//check the Japanese phrases
 		var key = document.getElementById('CatalogBoxText11').value;
+		var jpkey1= document.getElementById('CatalogBoxText9').value;
 		const jphrases = new Map();
 		jphrases.set("Screw back Water Proof","スクリューバック防水");
 		jphrases.set("Snap back water proof","スナップバック　防水");
@@ -282,11 +401,27 @@
 		jphrases.set("Water Proof","防水");
 		jphrases.set("Snap-back case (non-waterproof)","スナップバック　非防水");
 		jphrases.set("Screw-back case (waterproof)","スクリューバック防水");
+		jphrases.set("Original","オリジナル");
+		jphrases.set("Steel back","ｽﾃｨｰﾙバック");
+		jphrases.set("Screw non W/P","スクリューバック 非防水");
+		jphrases.set("Screw back w/proof","スクリューバック 防水");
+		jphrases.set("Snap-back,non-waterproof","スナップバック　非防水");
+		jphrases.set("Snap-back,Non-Waterproof","スナップバック　非防水");
+		jphrases.set("Snap-Back,Non-Waterproof","スナップバック　非防水");
+		jphrases.set("Snap-back Waterproof","スナップバック　防水");
+		jphrases.set("Snap-back (water-resistant)","スナップバック　防水");
+		jphrases.set("Screw back w/p","スナップバック　防水");
+		jphrases.set("Screw back W/proof","スナップバック　防水");
 		
 		if(jphrases.has(key)){
 			document.getElementById('CatalogBoxText11').value=jphrases.get(key);
 		}else{
 			alert("Enter in the new key/Value for the Japanese case field Kyle");
+		}
+		if(jphrases.has(jpkey1)){
+			document.getElementById('CatalogBoxText9').value=jphrases.get(jpkey1);
+		}else{
+			alert("text9 no key");
 		}
 		
 		//check Eng phrases
@@ -340,7 +475,14 @@
 		enphrases.set("ノン オリジナル （ベルト幅：16mm） /  GF Type-4","Non-original (Lug width: 16mm) /GF Type-4");
 		enphrases.set("Snapback, non-waterproof","Snap back Non-Water-resistant");
 		enphrases.set("Snap back, non-waterproof","Snap back Non-Water-resistant");
+		enphrases.set("Snap-back,non-waterproof","Snap back Non-Water-resistant");
 		enphrases.set("Snap back non w/p","Snap back Non-Water-resistant");
+		enphrases.set("Screw non W/P","Screw Back Non-Water-resistant");
+		enphrases.set("Screw back w/proof","Screw Back Water-resistant");
+		enphrases.set("Snap-Back,Non-Waterproof","Snap back Non-Water-resistant");
+		enphrases.set("Snap-back Waterproof","Snap back Water-resistant");
+		enphrases.set("Screw back w/p","Screw back Water-resistant");
+		enphrases.set("Screw back W/proof","Screw back Water-resistant");
 		
 		
 		if(enphrases.has(key1)){
