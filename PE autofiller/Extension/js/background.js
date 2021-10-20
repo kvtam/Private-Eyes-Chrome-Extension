0b1f115c84d05e9bb89f0dced5803765e4d8569a
kvtam <kvtam@alaska.edu>
2021-10-21 01:05:09 +0900

    removed debug message

--- PE autofiller/Extension/js/background.js
+++ PE autofiller/Extension/js/background.js
@@ -1,5 +1,4 @@
 chrome.commands.onCommand.addListener(function(command){
-	alert("lol");
 	chrome.tabs.executeScript(null, {file: "js/auto.js"});
 });
 chrome.browserAction.onClicked.addListener(function(tab) {// Clicking on the button on the extension bar runs the script
