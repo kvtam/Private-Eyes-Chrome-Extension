2ce8dd61d8f6c571927635a22fa2e8fb0ee58547
kvtam <kvtam@alaska.edu>
2021-10-26 12:07:43 +0900

    translator added

--- PE autofiller/Extension/js/background.js
+++ PE autofiller/Extension/js/background.js
@@ -1,6 +1,11 @@
+
+	
 chrome.commands.onCommand.addListener(function(command){
-	chrome.tabs.executeScript(null, {file: "js/auto.js"});
+	chrome.tabs.executeScript(null, {file: "js/translator.js"});
+	
 });
 chrome.browserAction.onClicked.addListener(function(tab) {// Clicking on the button on the extension bar runs the script
    chrome.tabs.executeScript(null, {file: "js/auto.js"});
+	
 });
+
---
+++ PE autofiller/Extension/js/translator.js
@@ -0,0 +1,30 @@
+
+ 
+translator(document.getElementById('CatalogBoxHtml2').value);
+function translator(inputText){ //Function to translate text and put into string using the deepL api 
+		var url = "https://api-free.deepl.com/v2/translate";
+
+		var xhr = new XMLHttpRequest();
+		xhr.open("POST", url);
+		xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+		xhr.onreadystatechange = function () {
+			if (xhr.readyState === 4) {
+				console.log(xhr.status);
+				console.log(xhr.responseText);
+				//return xhr.responseText;
+				var reg=/(?<="text":").*(?=")/;
+				 rx =xhr.responseText.match(reg);
+				 rx=rx.toString();
+				 rx=rx.replace(/CASE/i,"Case");
+				 rx=rx.replace(/DIAL/i,"Dial");
+				 rx=rx.replace(/Movement/i,"Movement");
+				alert(rx);
+					
+			}
+		};
+		
+		var data = "auth_key=&text="+inputText+"&target_lang=EN";//Put your own DeepL API key here, sign up on te
+		xhr.send(data);
+			
+}
+
